9776f0f907d1c594abae6f5f7a523bab3a72a7a9
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-07-24 18:08:14 +0900

    feat: add devShell (nixfmt) and .envrc for direnv

--- flake.nix
+++ flake.nix
@@ -32,5 +32,8 @@
         })
       ];
     };
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [ nixfmt ];
+    };
   };
 }
