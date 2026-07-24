55e3321ae40b94eb92bbf967e5a4ca6bda9c1027
yuitaa <95457974+yuitaa@users.noreply.github.com>
2026-07-25 00:56:48 +0900

    feat: add nvim-cmp (with buffer/lsp/path/emoji sources) and nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -80,4 +80,47 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/vim-vsnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+          { name = "emoji" },
+        }),
+      })
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        filters = { dotfiles = false },
+      })
+      vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+    end,
+  },
 })
